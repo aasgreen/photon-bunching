ab69ea65d108e1bc24ebc5d2bea51ffa48079ac6
ringstadd <ringstadd@hotmail.com>
2012-06-13 13:55:27 -0600

    Fixed normalization problems, produce more correct results, still need to update derivation to include normalization

--- probability_functions.py
+++ probability_functions.py
@@ -3,31 +3,44 @@
 import math
 
 import matplotlib.pyplot as plt
+
+def normalize(tau):
+	if tau == 0.:
+		return 1
+	else:
+		return (1./(math.exp(2.*tau)-1.)**2)
+
+def psi11norm(tau):
+	return math.exp(-2.*tau)*(math.exp(tau)-2.*math.exp(-tau))**2
 def psi11(tau):
-	return (-math.exp(-2.*tau)+1.)*(-math.exp(tau)+2.*math.exp(-tau))**2
+	return (-math.exp(-2.*tau)+1.)*(-math.exp(tau)+2.*math.exp(-tau))**2 * normalize(tau)
 
 def psi11_atom(tau):
-	return (-math.exp(tau)+2.*math.exp(-tau))**2
+	return (-math.exp(tau)+2.*math.exp(-tau))**2 *normalize(tau)
 
 def psi11_field(tau):
-	return	(-math.exp(-2.*tau)+1.)
+	return	(-math.exp(-2.*tau)+1.) * normalize(tau)
 
 def psi11_amp_field(tau):
-	return math.exp(-2*tau)
+	return math.exp(-2*tau) * normalize(tau)
 
 
 def psi20(tau):
-	return 2.*(1. -math.exp(-2.*tau))**2
+	return 2.*(1. -math.exp(-2.*tau))**2 
 
+def psi20norm(tau):
+	return 2*(1.-math.exp(-2.*tau))*math.exp(-2.*tau)
+def psi02norm(tau):
+	return 2*(1.-math.exp(-2.*tau))*math.exp(-2.*tau)
 def psi02(tau):
-	return 2.*( 1. -math.exp(-2.*tau))**2
+	return 2.*( 1. -math.exp(-2.*tau))**2 *normalize(tau)
 
 def exptest(t):
 	data = math.exp(3)
 	return data
 
 def prob_check (t):
-	return psi11(t)+psi20(t)+psi02(t)
+	return psi11norm(t)+psi20norm(t)+psi02norm(t)
 
 def plot_prob(funcList, ylim):
 	yList = [i/500.*ylim for i in range(500)]
@@ -37,5 +50,6 @@ def plot_prob(funcList, ylim):
 	plt.legend()
 	plt.xlabel(r'$\tau$')
 	plt.ylabel('Probability')
+	plt.ylim([0,3])
 	return 0
 		
